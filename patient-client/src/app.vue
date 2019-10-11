a7868e2784f20f2093d0848326c0662343f3d4a1
Kousuke Takahashi <42241308+panpukin1111@users.noreply.github.com>
2019-10-11 17:21:12 +0900

    Update app.vue

--- patient-client/src/app.vue
+++ patient-client/src/app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <!-- <router-link to="/">top---------------2</router-link>
-    <router-link to="/about">about</router-link> -->
     <router-view></router-view>
   </div>
 </template>
