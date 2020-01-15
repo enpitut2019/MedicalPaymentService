63865ebf3655a04e47e23fa9d0f0421ea2c643f5
Kota <kcm@outlook.jp>
2020-01-15 07:18:49 +0000

    概要の翻訳を追加

--- patient-client/src/top.vue
+++ patient-client/src/top.vue
@@ -17,18 +17,9 @@
                 医療費を暗号通貨で支払うことを想定したサービスです。
                 電子カルテなど既存サービスとの共存を前提としています。
                 サービスのメリットとしては、
-            </p>
-            <p>
                 ①診療ごとに専用のアドレスを発行することによる病院側の管理コスト軽減
-            </p>
-            <p>
                 ②第三者機関を介さない前払いの管理
-            </p>
-            <p>
-                ③伝達ミスを原因とするトラブルの客観的解決
-            </p>
-            <p>
-                などが挙げられます。
+                ③伝達ミスを原因とするトラブルの客観的解決 などが挙げられます。
             </p>
         </div>
         <div class="center">
@@ -65,6 +56,8 @@ export default {
     },
     locales: {
         en: {
+            service_text:
+                "This service assumes that medical expenses will be paid using cryptocurrencies. It is premised on coexistence with existing services such as electronic medical records. Advantages of the service are: ①Reduction of management costs on the hospital side by issuing a dedicated address for each medical treatment ②Management of prepayment without a third-party organization ③Objective resolution of troubles caused by transmission errors",
             button_text1: "Enter Your Information",
             button_text2: "Edit Your Information",
             info_text0: "1. Enter/Edit your information from the below button.",
@@ -73,6 +66,8 @@ export default {
                 "Do not share this QR code on the Internet because it contains personal information!"
         },
         ja: {
+            service_text:
+                "医療費を暗号通貨で支払うことを想定したサービスです。電子カルテなど既存サービスとの共存を前提としています。サービスのメリットとしては ①診療ごとに専用のアドレスを発行することによる病院側の管理コスト軽減 ②第三者機関を介さない前払いの管理 ③伝達ミスを原因とするトラブルの客観的解決 などが挙げられます。",
             button_text1: "受付情報の入力",
             button_text2: "入力した情報の修正",
             info_text0: "1. 下のボタンからあなたの情報を入力/編集してください",
@@ -81,6 +76,8 @@ export default {
                 "このQRコードは個人情報を含むため、インターネットで公開しないでください!"
         },
         ru: {
+            service_text:
+                "Данная услуга предполагает, что медицинские расходы будут оплачены с использованием криптовалют. Он основан на сосуществовании с существующими службами, такими как электронные медицинские карты. Преимущества услуги: edСнижение административных расходов на стороне больницы путем выдачи выделенного адреса для каждого лечения. MedicalУправление предоплатой без сторонней организации. BОбъективное решение проблем, вызванных ошибками при передаче.",
             button_text1: "Введите информацию о приеме",
             button_text2: "Исправьте введенную информацию",
             info_text0:
